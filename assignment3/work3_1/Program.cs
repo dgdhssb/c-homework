9cb6e15c45b588e272182facdb63e40cb9fa5719
dgdhssb <134912711+dgdhssb@users.noreply.github.com>
2024-03-13 22:12:53 +0800

    Add files via upload

---
+++ assignment3/work3_1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace work3_1
+{
+    public interface Shape
+    {
+        double Area();
+        bool IsLegal();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double length;
+        public double width;
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double Area()
+        {
+            return length * width;
+        }
+
+        public bool IsLegal()
+        {
+            return length > 0 && width > 0;
+        }
+        
+    }
+
+    public class Square : Shape
+    {
+        public double side;
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public double Area() { return side * side; }
+
+        public bool IsLegal() { return side > 0; }
+    }
+
+    public class Triangle : Shape
+    {
+        public double firstEdge;
+        public double secondEdge;
+        public double thirdEdge;
+
+        public Triangle(double firstEdge, double secondEdge, double thirdEdge)
+        {
+            this.firstEdge = firstEdge;
+            this.secondEdge = secondEdge;
+            this.thirdEdge = thirdEdge;
+        }
+
+        public double Area() 
+        { 
+            double p=(firstEdge+secondEdge+thirdEdge)/2;
+            double ss=p*(p-firstEdge)*(p-secondEdge)*(p-thirdEdge);
+            double s=Math.Sqrt(ss);
+            return s;
+        }
+        public bool IsLegal() 
+        {
+            return firstEdge>0&&secondEdge>0&&thirdEdge>0&&((firstEdge+secondEdge>thirdEdge)||(firstEdge+thirdEdge>secondEdge)
+                ||(secondEdge+thirdEdge>firstEdge));
+        }
+    }
+
+    public class ExpressThem
+    {
+        public void ToString(Shape shape)
+        {
+            Console.WriteLine("图形是否合理：" + shape.IsLegal());
+            if(!shape.IsLegal()) { throw new Exception("图形不合理！！！"); }
+            Console.WriteLine("图形的面积为：" + shape.Area());
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressThem expressThem = new ExpressThem();
+            try
+            {
+                Rectangle rectangle = new Rectangle(4.0, 5.0);
+                Console.WriteLine("矩形：");
+                expressThem.ToString(rectangle);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                Square square = new Square(-1.0);
+                Console.WriteLine("正方形：");
+                expressThem.ToString(square);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                Triangle triangle = new Triangle(3.0, 4.0, 5.0);
+                Console.WriteLine("三角形：");
+                expressThem.ToString(triangle);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ assignment3/work3_2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace work3_2
+{
+    public interface AnyShape
+    {
+        double Area();
+    }
+
+    public class Rectangle : AnyShape
+    {
+        public double Length { get; set; }
+        public double Width {  get; set; }
+       
+        public double Area()
+        {
+            return Length * Width;
+        }
+    }
+
+    public class Square : AnyShape
+    {
+        public double Side { set; get; }
+        public double Area() { return Side * Side; }
+    }
+
+    public class Triangle : AnyShape
+    {
+        public double Tbase { set; get; }
+        public double Height { set; get; }
+        public double Area() { return Height*Tbase/2; }
+    }
+
+    public class FactorShape
+    {
+        readonly Random random = new Random();
+        public double GetNumber()
+        {
+            return random.Next(5,20);
+        }
+        public AnyShape CreatShape(string shapeName)
+        {
+            switch(shapeName)
+            {
+                case "Rectangle":
+                    return new Rectangle { Width = GetNumber(),Length=GetNumber() };
+                case "Square":
+                    return new Square { Side = GetNumber() };
+                case "Triangle":
+                    return new Triangle { Height = GetNumber(),Tbase=GetNumber() };
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorShape factorShape = new FactorShape();
+            double totalArea=0;
+            string[] shapes = { "Rectangle", "Square", "Triangle" };
+            Random random = new Random();
+            for (int i = 0;i<10;i++)
+            {
+                int luckyNum=random.Next(3);
+                AnyShape shape = factorShape.CreatShape(shapes[luckyNum]);
+                Console.WriteLine($"第{i + 1}个形状是：{shapes[luckyNum]} 面积为：{shape.Area()}");
+                totalArea += shape.Area();
+            }
+            Console.WriteLine("十个对象形状的面积和为： " + totalArea);
+            Console.ReadKey();
+        }
+    }
+}

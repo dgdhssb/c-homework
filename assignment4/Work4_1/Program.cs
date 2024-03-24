497dce6ddf7d24a72e5f32f287e3e123d1e2a981
dgdhssb <134912711+dgdhssb@users.noreply.github.com>
2024-03-24 22:54:11 +0800

    Add files via upload

---
+++ assignment4/Work4_1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+//通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。
+
+namespace Work4_1
+{
+    public class Node<T>
+    {
+        public Node<T> Next{ set; get; }
+        public T Data { set; get; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get=> head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n=new Node<T>(t);
+            if (tail == null)
+            {
+                head=tail = n;
+            }else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> genericList = new GenericList<int>();
+            genericList.Add(1);
+            genericList.Add(2);
+            genericList.Add(3);
+            genericList.Add(4);
+            genericList.ForEach(x=>Console.WriteLine(x));
+            int sum = 0;
+            genericList.ForEach(x=>sum+=x);
+            int max = genericList.Head.Data;
+            genericList.ForEach(x => max = (x > max) ? x:max) ;
+            int min = genericList.Head.Data;
+            genericList.ForEach(x => min = (x < min) ? x:min) ;
+            Console.WriteLine($"总和是{sum} 最大值是{max} 最小值是{min}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignment4/Work4_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work4_2
+{
+    public delegate void EventHandler(object sender, EventArgs e);
+    public class EventArgs { 
+        public int A { get; set; }
+    }
+
+    public class Clock
+    {
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+        private int times;
+
+        public Clock(int a) 
+        {
+            times = a;
+            EventArgs eventArgs = new EventArgs()
+            {
+                A = a
+            };
+        }
+
+        public void Start() 
+        {
+            Console.WriteLine("闹钟开始运行");
+            for (int i = 0; i < times; i++)
+            {
+                OnTick();
+                Console.WriteLine($"第{i + 1}秒");
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+            }
+            OnAlarm();
+        }
+
+        public void OnTick()
+        {
+            EventArgs eventArgs = new EventArgs();
+            if (Tick != null)
+            {
+                Tick(this, eventArgs);
+            }
+        }
+
+        public void OnAlarm()
+        {
+            EventArgs eventArgs = new EventArgs();
+            if(Alarm != null)
+            {
+                Alarm(this, eventArgs);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock=new Clock(12);
+            clock.Tick += (sender, e) => Console.WriteLine("滴答滴答...");
+            clock.Alarm += (sender, e) => Console.WriteLine("闹钟铃声响了...");
+            clock.Start();
+            //Console.ReadKey();
+        }
+    }
+}

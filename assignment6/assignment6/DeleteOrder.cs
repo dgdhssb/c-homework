6de5dfb87a89f667849ede651bca06f409cec632
dgdhssb <134912711+dgdhssb@users.noreply.github.com>
2024-04-09 17:15:30 +0800

    Add files via upload

---
+++ assignment6/assignment6/DeleteOrder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment6
+{
+    public partial class DeleteOrder : Form
+    {
+        public DeleteOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void AllOrderLabel_Click(object sender, EventArgs e)
+        {
+            foreach(Order order in AddOrderHHH.Orders)
+            {
+                AllOrderlistBox.Items.Add(order);
+            }
+        }
+
+        private void AllOrderlistBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChooseDeleteButton_Click(object sender, EventArgs e)
+        {
+            List<Order> ordersToDelete = new List<Order>();
+
+            foreach (Order order in AllOrderlistBox.SelectedItems)
+            {
+                ordersToDelete.Add(order);
+            }
+
+            foreach (Order order in ordersToDelete)
+            {
+                AddOrderHHH.Orders.Remove(order);
+                AllOrderlistBox.Items.Remove(order);
+            }
+        }
+    }
+}
---
+++ assignment6/assignment6/OrderSystem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment6
+{
+    public partial class OrderSystem : Form
+    {
+        public OrderSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddOrderHHH addOrderHHH = new AddOrderHHH();
+            this.Hide();
+            addOrderHHH.ShowDialog();
+            this.Show();
+        }
+
+        private void OrderSystem_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GoodsButton_Click(object sender, EventArgs e)
+        {
+            AllGoods goods = new AllGoods();
+            this.Hide();
+            goods.ShowDialog();
+            this.Show();
+        }
+
+        private void FindButton_Click(object sender, EventArgs e)
+        {
+            FindOrders findOrders = new FindOrders();
+            this.Hide();    
+            findOrders.ShowDialog();
+            this.Show();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            DeleteOrder deleteOrder = new DeleteOrder();
+            this.Hide();    
+            deleteOrder.ShowDialog();
+            this.Show();
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            ChangeOrder changeOrder = new ChangeOrder();
+            this.Hide();
+            changeOrder.ShowDialog();
+            this.Show();
+        }
+    }
+}
